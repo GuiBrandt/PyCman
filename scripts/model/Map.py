d8d4eeb8606c649d4604e31d29b8b504a7590c88
GUILHERME GUIDOTTI BRANDT <u16173@academico.cotuca.unicamp.br>
2018-11-09 09:26:27 -0200

    Coisas no mapa

--- scripts/model/Map.py
+++ scripts/model/Map.py
@@ -1,6 +1,25 @@
+import io
+
 class Map:
-
-    def __init__():
+    def __init__(filename):
         """Construtor"""
+        __map = 
 
+    def __get_tile_at(x, y):
+        return __map
+
+    def is_passable(x, y):
+        """
+            Determina se uma posição no mapa é passável
         
+            x, y    : Coordenadas no mapa
+        """
+        return __get_tile_at(x, y) != 'x'
+
+    def get_tile(x, y):
+        """
+            Obtém o tipo de tile numa posição do mapa
+
+            x, y    : Coordenadas no mapa
+        """
+        return __get_tile_at(x, y)
