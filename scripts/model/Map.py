d47432d546d0ce06e3a5e4e421d0aae309be51e7
GUILHERME GUIDOTTI BRANDT <u16173@academico.cotuca.unicamp.br>
2018-11-09 11:01:08 -0200

    Arquivo de passabilidade melhorado

--- scripts/model/Map.py
+++ scripts/model/Map.py
@@ -1,12 +1,47 @@
 import io
+from enum import Enum
 
 class Map:
+    """Classe de modelo de mapa do jogo"""
+
+    class Tile(Enum):
+        """Enumeração de tipos de tile no mapa"""
+        EMPTY = 0
+        WALL_S = 2
+        WALL_D = 3
+        POINT = 4
+        BONUS = 5
+
+    # Mapa de caracteres do arquivo para tipos de tile
+    __TILE_DICT = {
+        '\'':   Tile.EMPTY,
+        's':    Tile.WALL_S,
+        'd':    Tile.WALL_D,
+        '.':    Tile.POINT,
+        'o':    Tile.BONUS
+    }
+
     def __init__(filename):
-        """Construtor"""
-        __map = 
+        """
+            Construtor
+
+            filename    : Nome do arquivo de passabilidade do mapa
+        """
+
+        file = io.open(filename, "r")
+        __width, __height = map(int, file.readline().split())
+        
+        __map = ""
+        for i in range(__height):
+            __map += file.readline().rstrip("\n\r")
 
     def __get_tile_at(x, y):
-        return __map
+        """
+            Obtém o tipo de tile em uma posição no mapa
+
+            x, y    : Coordenadas no mapa
+        """
+        return __map[y * __width + x]
 
     def is_passable(x, y):
         """
@@ -14,7 +49,7 @@ class Map:
         
             x, y    : Coordenadas no mapa
         """
-        return __get_tile_at(x, y) != 'x'
+        return __get_tile_at(x, y).lower() != 'x'
 
     def get_tile(x, y):
         """
@@ -22,4 +57,4 @@ class Map:
 
             x, y    : Coordenadas no mapa
         """
-        return __get_tile_at(x, y)
+        return __TILE_DICT[__get_tile_at(x, y)]
